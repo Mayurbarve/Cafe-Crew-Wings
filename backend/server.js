9ff5b6f0181000225b947ca3b6cf2dede77d588b
Mayurbarve <mayurbarve04@gmail.com>
2025-06-03 10:46:07 +0530

    Remove auth

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express  from "express"
 import cors from 'cors'
 import { connectDB } from "./config/db.js"
-import userRouter from "./routes/userRoute.js"
 import foodRouter from "./routes/foodRoute.js"
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
@@ -12,15 +11,11 @@ const app = express()
 const port = process.env.PORT || 4000;
 
 
-// middlewares
-app.use(express.json())
-app.use(cors())
 
 // db connection
 connectDB()
 
 // api endpoints
-app.use("/api/user", userRouter)
 app.use("/api/food", foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/cart", cartRouter)
