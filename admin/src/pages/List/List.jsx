32b499966d2f94c9c285793ef135b8fac44aa5c6
Mayurbarve <mayurbarve04@gmail.com>
2025-06-16 21:04:47 +0530

    Image issu fix

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,46 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import './List.css'
-import { currency } from '../../assets/assets'
+import React, { useEffect, useState } from 'react';
+import './List.css';
+import { currency } from '../../assets/assets';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const List = () => {
-
   const url = import.meta.env.VITE_BACKEND_URL;
-
   const [list, setList] = useState([]);
 
   const fetchList = async () => {
-    const response = await axios.get(`${url}/api/food/list`)
-    if (response.data.success) {
-      setList(response.data.data);
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+      if (response.data.success) {
+        setList(response.data.data);
+      } else {
+        toast.error("Error");
+      }
+    } catch (err) {
+      toast.error("Server Error");
     }
-    else {
-      toast.error("Error")
-    }
-  }
+  };
 
   const removeFood = async (foodId) => {
-    const response = await axios.post(`${url}/api/food/remove`, {
-      id: foodId
-    })
-    await fetchList();
-    if (response.data.success) {
-      toast.success(response.data.message);
+    try {
+      const response = await axios.post(`${url}/api/food/remove`, {
+        id: foodId,
+      });
+      await fetchList();
+      if (response.data.success) {
+        toast.success(response.data.message);
+      } else {
+        toast.error("Error");
+      }
+    } catch (err) {
+      toast.error("Server Error");
     }
-    else {
-      toast.error("Error")
-    }
-  }
+  };
 
   useEffect(() => {
     fetchList();
-  }, [])
+  }, []);
 
   return (
-    <div className='list add flex-col'>
+    <div className="list add flex-col">
       <p>All Foods List</p>
-      <div className='list-table'>
+      <div className="list-table">
         <div className="list-table-format title">
           <b>Image</b>
           <b>Name</b>
@@ -48,20 +52,18 @@ const List = () => {
           <b>Price</b>
           <b>Action</b>
         </div>
-        {list.map((item, index) => {
-          return (
-            <div key={index} className='list-table-format'>
-              <img src={`${url}/images/` + item.image} alt="" />
-              <p>{item.name}</p>
-              <p>{item.category}</p>
-              <p>{currency}{item.price}</p>
-              <p className='cursor' onClick={() => removeFood(item._id)}>x</p>
-            </div>
-          )
-        })}
+        {list.map((item, index) => (
+          <div key={index} className="list-table-format">
+            <img src={item.image} alt={item.name} />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>{currency}{item.price}</p>
+            <p className="cursor" onClick={() => removeFood(item._id)}>x</p>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
